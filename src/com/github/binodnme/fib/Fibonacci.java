9a453af8fb4cb29562abafe26384d78fcace7c60
Binod Shrestha <binodshrestha@lftechnology.com>
2016-10-25 21:57:33 +0545

    project init

---
+++ src/com/github/binodnme/fib/Fibonacci.java
@@ -0,0 +1,42 @@
+package com.github.binodnme.fib;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lftech on 10/25/2016.
+ */
+public class Fibonacci {
+    private static Map<Integer, Long> map = new HashMap<>();
+
+
+    public static long fib(int n) {
+        if (n <= 2) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static long fib1(int n) {
+        long a = 1;
+        long b = 1;
+
+        for (int i = 3; i <= n; i++) {
+            long temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b;
+    }
+
+    public static long fib2(int n) {
+        Long result = map.get(n);
+        if (result != null) return result;
+        map.put(n, fib2(n - 1) + fib2(n - 2));
+        return map.get(n);
+    }
+
+    public static void main(String[] args) {
+        map.put(1, 1L);
+        map.put(2, 1L);
+        System.out.println(fib2(1000));
+    }
+}
